c1b82b0da04a2b30abb4729626ac97adc754172f
james07252001 <pastorillo.james25@gmail.com>
2024-07-21 21:04:03 +0800

    commit

--- admin/dbconfig.php
+++ admin/dbconfig.php
@@ -1,13 +1,15 @@
 <?php
-    $servername = "localhost";
-    $username = "u510162695_bobrs";
-    $password = "1Bobrs_password";
-    $dbname = "u510162695_bobrs";
+$servername = "localhost";
+$username = "u510162695_bobrs";
+$password = "1Bobrs_password";
+$dbname = "u510162695_bobrs";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
