d61c4641787b779e3b15b9b2cfa9963fc11ce23e
james07252001 <pastorillo.james25@gmail.com>
2024-07-21 20:37:24 +0800

    commit

--- controllers/location.php
+++ controllers/location.php
@@ -1,4 +1,17 @@
 <?php
+
+$route_from = isset($_GET['route_from']) && !empty($_GET['route_from']) ? $_GET['route_from'] : "";
+$route_to = isset($_GET['route_to']) && !empty($_GET['route_to']) ? $_GET['route_to'] : "";
+$schedule_date = isset($_GET['schedule_date']) && !empty($_GET['schedule_date']) ? $_GET['schedule_date'] : "";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$new_location = new Location($db);
+$locations = $new_location->getAll();
+$location_from = $new_location->getByLocation($route_from);
+$location_to = $new_location->getByLocation($route_to);
+
     class Location
     {
         private $conn;
@@ -40,16 +53,4 @@
     }
 
     
-    $route_from = isset($_GET['route_from']) && !empty($_GET['route_from']) ? $_GET['route_from'] : "";
-    $route_to = isset($_GET['route_to']) && !empty($_GET['route_to']) ? $_GET['route_to'] : "";
-    $schedule_date = isset($_GET['schedule_date']) && !empty($_GET['schedule_date']) ? $_GET['schedule_date'] : "";
-
-    $database = new Database();
-    $db = $database->getConnection();
-
-    $new_location = new Location($db);
-    $locations = $new_location->getAll();
-    $location_from = $new_location->getByLocation($route_from);
-    $location_to = $new_location->getByLocation($route_to);
-
 ?>
