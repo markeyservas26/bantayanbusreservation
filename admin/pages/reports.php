85daf4ca6cb4c9ec9bbab027737196f755127a00
james07252001 <pastorillo.james25@gmail.com>
2024-07-21 19:25:17 +0800

    commit all

--- admin/pages/reports.php
+++ admin/pages/reports.php
@@ -1,6 +1,6 @@
 <?php
     // Database connection
-    $conn = mysqli_connect('localhost', 'root', '', 'bobrs');
+    $conn = mysqli_connect('localhost', 'u510162695_bobrs', '1Bobrs_password', 'u510162695_bobrs');
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
