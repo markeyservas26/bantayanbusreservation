d609dfdb35ed4dcc5225fbe13909ebe9e2344d90
james07252001 <pastorillo.james25@gmail.com>
2024-07-27 00:36:23 +0800

    commit

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -193,10 +193,9 @@
         <div class="col-6 col-sm-4 col-md-3 mb-3">
     <div class="bg-white shadow border-top p-3 border-primary rounded h-100" style="background-image: linear-gradient(to top, #fddb92 0%, #d1fdff 100%);">
         <div class="d-flex align-items-center">
+            <!-- SVG icon for peso sign -->
             <svg xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 0 24 24" width="50px" fill="#000000">
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zM7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 2.88-2.88 7.19-5 9.88C9.92 16.21 7 11.85 7 9z" />
-                <circle cx="12" cy="9" r="2.5" />
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v2h-2v-2zm0 4h2v8h-2v-8z"/>
             </svg>
             <div class="text-center w-100">
                 <p class="mb-0" style="font-family: 'Times New Roman', serif;"><b>TOTAL FARE</b></p>
@@ -212,6 +211,7 @@
         </div>
     </div>
 </div>
+
 </div>
 
     <!-- Chart.js canvas -->
