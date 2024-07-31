2c3ff1d52e747b9954bc0bce359398b3e4af9947
james07252001 <pastorillo.james25@gmail.com>
2024-07-31 20:52:28 +0800

    commit

--- register.php
+++ register.php
@@ -52,7 +52,7 @@
 
                 <form method="POST" action="" >
                     <div class="form-row mb-3">
-                        <div class="col-md-6">
+                        <div class="form-group">
                             <label for="first_name" style="color: white; font-weight: bold">First Name</label>
                             <input type="text" class="form-control" id="first_name" name="first_name" required />
                         </div>
