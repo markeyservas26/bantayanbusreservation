9b3e067a4d75fbf3da03a8aa86f61d267ca0236b
james07252001 <pastorillo.james25@gmail.com>
2024-08-01 10:53:32 +0800

    commit

--- login.php
+++ login.php
@@ -25,7 +25,7 @@
 <main>
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-6 d-flex align-items-center" style="margin-left: -150px">
+            <div class="col-md-6 d-flex align-items-center" style="margin-left: -150px; width: 350px">
                 <div class="w-100 bg-white shadow-sm p-3" style="max-width: 500px;">
                     <div class="bg-primary p-3" style="background-image: linear-gradient(109.6deg, rgba(254,253,205,1) 11.2%, rgba(163,230,255,1) 91.1%);">
                         <h1 class="text-center">Login</h1>
