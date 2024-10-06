5a48eba8cd251adbe9a232a35816887466cae1b1
james07252001 <pastorillo.james25@gmail.com>
2024-10-06 20:13:23 +0800

    commit

--- controllers/db.php
+++ controllers/db.php
@@ -1,9 +1,9 @@
 <?php
     class Database{
         private $host = "localhost";
-        private $db_name = "bobrs";
-        private $username = "root";
-        private $password = "";
+        private $db_name = "u510162695_bobrs";
+        private $username = "u510162695_bobrs";
+        private $password = "1Bobrs_password";
         public $conn;
 
         public function getConnection(){
